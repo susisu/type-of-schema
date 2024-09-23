dc4a356f431b04da1a8c1386b10e9696e1d7c341
Susisu <susisu2413@yahoo.co.jp>
2024-09-23 23:16:25 +0900

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,22 @@
 import globals from "globals";
-import { config, map } from "@susisu/eslint-config";
-import prettierConfig from "eslint-config-prettier";
+import { config } from "@susisu/eslint-config";
 
-export default [
-  ...map({ files: ["src/**/*.ts"] }, [
-    config.tsTypeChecked(),
-    {
-      languageOptions: {
-        sourceType: "module",
-        parserOptions: {
-          project: "./tsconfig.json",
-        },
-        globals: {
-          ...globals.es2021,
-        },
+export default config({ tsconfigRootDir: import.meta.dirname }, [
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.es2023,
       },
     },
-  ]),
-  ...map({ files: ["*.js"] }, [
-    config.js(),
-    {
-      languageOptions: {
-        sourceType: "module",
-        globals: {
-          ...globals.es2021,
-          ...globals.node,
-        },
+  },
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.es2023,
+        ...globals.node,
       },
     },
-  ]),
-  prettierConfig,
-];
+  },
+]);
